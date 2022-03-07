38047bc9015b6e60f23326803bb7ddf63baa9467
toofantah <toofan.tah@gmail.com>
2022-03-07 21:38:55 +0300

    Defining Walls And Corridors
    Update : Maze.cs

--- Assets/Script/Maze.cs
+++ Assets/Script/Maze.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,19 +9,61 @@ public class Maze : MonoBehaviour
     public int width = 30;
     public int depth = 30;
 
+    public byte[,] map;
+
     // Start is called before the first frame update
     void Start()
+    {
+        InitialzeMap();
+        Generate();
+        DrawMap();
+    }
+
+   
+
+    private void InitialzeMap()
+    {
+        map = new byte[width, depth];
+        for (int X = 0; X < depth; X++)
+        {
+            for (int Z = 0; Z < width; Z++)
+            {
+                map[X, Z] = 1; // 1 = wall 0 = corridor;
+
+            }
+        }
+    }
+
+    private void Generate()
     {
         for (int X = 0; X < depth; X++)
         {
             for (int Z = 0; Z < width; Z++)
             {
-                Vector3 pos = new Vector3(X, 0, Z);
-                Instantiate(cube, pos, Quaternion.identity);
+                if(UnityEngine.Random.Range(0,100)<50)
+                    map[X, Z] = 0; // 1 = wall 0 = corridor;
 
             }
         }
     }
+    private void DrawMap()
+    {
+        for (int X = 0; X < depth; X++)
+        {
+            for (int Z = 0; Z < width; Z++)
+            {
+                if (map[X, Z] == 1) 
+                {
+                    Vector3 pos = new Vector3(X, 0, Z);
+                    GameObject wall = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    wall.transform.position = pos;
+                }
+
+            }
+        }
+    }
+
+   
 
     // Update is called once per frame
     void Update()
